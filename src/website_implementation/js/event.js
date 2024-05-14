a6031059f093891603f6debabc2715821a6bc482
mike9025 <chinhao.lee@uqconnect.edu.au>
2024-05-14 11:19:12 +1000

    finish the basic of the event page

--- src/website_implementation/js/event.js
+++ src/website_implementation/js/event.js
@@ -1,25 +1,37 @@
 
 
 
-  
-   const my_website_code = 'Pete123'
-   const queryParams = {
-       website_code: my_website_code,
-   }
-   const baseURL = "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/community_events/";
-   const queryString = new URLSearchParams(queryParams).toString();
-   const urlWithParams = baseURL + "?" + queryString;
-   const requestOptions = {
-       method: 'GET',
-       redirect: 'follow'
-   };
-   fetch(urlWithParams, requestOptions)
-       .then(response => response.json())
-       .then(data => {
-           data.forEach(event => {
-               const markup = `<li>${event.name}</li>`;
-               document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
-               
-           });
-           console.log(data)
-       });
+
+const my_website_code = 'Pete123'
+const queryParams = {
+    website_code: my_website_code,
+}
+const baseURL = "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/community_events/";
+const queryString = new URLSearchParams(queryParams).toString();
+const urlWithParams = baseURL + "?" + queryString;
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+fetch(urlWithParams, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(event => {
+            photo = event.photo
+            if (event.photo == null) {
+                event.photo = "https://deco7140sem22023.s3.amazonaws.com/event_photos/orca_YQtzNXS.jpg"
+            }
+            const markup = `
+               <div class="card">
+                   <img src="${event.photo}" alt="${event.name}">
+                   <div class="container">
+                       <h4><b>${event.name}</b></h4>
+                       <p>${event.description}</p>
+                   </div>
+               </div>
+           `;
+            document.querySelector('.card-container').insertAdjacentHTML('beforeend', markup);
+        });
+
+    });
